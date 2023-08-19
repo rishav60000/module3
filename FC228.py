3e15504b5efeef06917ba4f2c74be3c4d4a517fe
rishav60000 <142087181+rishav60000@users.noreply.github.com>
2023-08-19 17:37:29 +0530

    Update FC228.py

--- FC228.py
+++ FC228.py
@@ -3,7 +3,7 @@ ro=['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','III','II','I']
 de=[1000,900,500,400,100,90,50,40,10,9,5,4,3,2,1]
 row=''
 i=0
-while i<len(ro):
+while i<15:
     m=n//de[i]
     j=0
     while j<m:
