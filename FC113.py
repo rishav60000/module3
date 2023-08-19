87f91b65f882609330bbb71f75d3917f80f7c825
rishav60000 <142087181+rishav60000@users.noreply.github.com>
2023-08-19 17:39:56 +0530

    Update FC113.py

--- FC113.py
+++ FC113.py
@@ -1,22 +1,22 @@
-# num=int(input('>>'))
-# b=[0]*num
-# n=0
-# for i in range(num):
-#     a=int(input('>>'))
-#     b[i]=a
-#     n+=1
-# hcf=0
-# c=b[0]
-# for m in b:
-#     if m<c:
-#         c=m
-# print(c)
-# for x in range(1,c+1):
-#     count=0
-#     for y in b:
-#         if y%x==0:
-#             count+=1 
-#     if count==n:
-#         hcf=x
-# print(hcf)
+num=int(input('>>'))
+b=[0]*num
+n=0
+for i in range(num):
+    a=int(input('>>'))
+    b[i]=a
+    n+=1
+hcf=0
+c=b[0]
+for m in b:
+    if m<c:
+        c=m
+print(c)
+for x in range(1,c+1):
+    count=0
+    for y in b:
+        if y%x==0:
+            count+=1 
+    if count==n:
+        hcf=x
+print(hcf)
 
