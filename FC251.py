669aa37f3987d80a86ee83d4494a8cab9b4c54bd
rishav60000 <142087181+rishav60000@users.noreply.github.com>
2023-08-19 17:35:06 +0530

    Update FC251.py

--- FC251.py
+++ FC251.py
@@ -2,9 +2,9 @@ a=[[4,5],[6,7]]
 b=[[1,2],[3,8]]
 c=[[0,0],[0,0]]
 i=0
-while i<len(a):
+while i<2:
     j=0
-    while j<len(b):
+    while j<2:
         c[i][j]=a[i][j]+b[j][i]
         j+=1
     i+=1
